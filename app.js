3eac1ddfcbdf06a0b3cb532257038c6ce9eeb991
Saloni-Rathod <saloni@jbspl.com>
2018-11-05 18:54:42 +0530

    Upadated file
    Inserting of data is enabled using this file

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/js', express.static(path.join(`${__dirname}/node_modules/bootstrap/dis
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'], title: 'Library' });
+  res.render('index', { list: ['about', 'services','portfolio','pricing','contact','signup'], title: 'Library' });
 });
 
 
@@ -36,15 +36,51 @@ app.use(bodyParser.json())
 
 mongoose.Promise = global.Promise;
 
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log('Successfully connected to the database');
-}).catch((err) => {
-  console.log('Could not connect to the database. Exiting now...', err);
-  process.exit();
+mongoose.connect("mongodb://localhost:27017/webapplication");
+
+  var nameSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password:String
+
 });
+
+
+
+var comments = new mongoose.Schema({
+    name: String,
+    email: String,
+    comments:String
+
+});
+
+var User = mongoose.model("User", nameSchema);
+  var contact_us  = mongoose.model("contact_us", comments);
+app.post("/signup", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Successfully signed in");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to sign-in");
+        });
+});
+
+
+
+
+app.post("/contact_us", (req, res) => {
+    var myData = new contact_us(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Our team will soon get in touch with u");
+        })
+        .catch(err => {
+            res.status(400).send("Sorry unable to proceed");
+        });
+});
+
 
 app.listen(port, () => {
   console.log(`listening on port ${chalk.green('port')}`);
